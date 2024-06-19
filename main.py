93016472ba09b03e7a946b544251636b01458e54
Shane B. <dotfortun@users.noreply.github.com>
2024-06-19 23:00:13 +0000

    Initial example, simple contact app.

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
-hello = """Hello world.
-This is a dev container for a seminar hosted by 4Geeks Academy,
-where we'll be building a simple API using Flask.
-"""
+from flask import Flask, jsonify, request
 
-if __name__ == "__main__":
-    print(hello)
+app = Flask(__name__)
+
+"""
+Contact object:
+{
+    "id": 1,
+    "name": "Cat McCattington",
+    "phone": "123-456-7890",
+    "email": "mccattington@catmail.com",
+}
+"""
+contacts: list[dict] = [
+    {
+        "id": 1,
+        "name": "Cat McCattington",
+        "phone": "123-456-7890",
+        "email": "mccattington@catmail.com",
+    }
+]
+
+
+@app.route("/", methods=["GET", "POST"])
+def hello_world():
+    return """<h1>Hello world!</h1>"""
+
+
+@app.route("/error")
+def throw_error():
+    raise Exception("Oh no, something broke!")
+
+
+@app.route("/contacts/<int:id>", methods=["GET"])
+def read_one_contact(id: int):
+    contact = list(filter(
+        # (contact) => id === contact.id
+        lambda contact: id == contact["id"],
+        contacts
+    ))
+    if len(contact) > 0:
+        return jsonify(contact.pop())
+    return jsonify(msg="Contact not found"), 404
+
+
+@app.route("/contacts", methods=["GET"])
+def read_all_contacts():
+    return jsonify(contacts=contacts)
+
+
+@app.route("/contacts", methods=["POST"])
+def create_contact():
+    data = request.json
+    contact = {
+        **data,
+        "id": len(contacts) + 1
+    }
+    contacts.append(contact)
+    return jsonify(contact)
